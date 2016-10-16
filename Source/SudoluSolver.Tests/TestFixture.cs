6d959fa22a596200aa2dc8ca38b1df04393f3003
alexey <alexey@.mail.ru>
2016-10-16 04:45:52 +0300

    Должно работать примерно так

--- Source/SudoluSolver.Tests/TestFixture.cs
+++ Source/SudoluSolver.Tests/TestFixture.cs
@@ -23,43 +23,43 @@ namespace SudokuSolver.Tests
         }
 
         [Test]
-        public void Sudoku2x2Solver()
+        public void Sudoku2X2Solver()
         {
             var solver = new SudokuSolver();
 
-            var problem = new Square(3);
-            problem.Structure[1, 3] = 7;
-            problem.Structure[1, 5] = 1;
-            problem.Structure[1, 8] = 2;
-            problem.Structure[2, 2] = 9;
-            problem.Structure[2, 7] = 4;
-            problem.Structure[3, 0] = 8;
-            problem.Structure[3, 1] = 1;
-            problem.Structure[3, 5] = 7;
-            problem.Structure[3, 8] = 4;
-            problem.Structure[4, 3] = 2;
-            problem.Structure[4, 4] = 4;
-            problem.Structure[4, 6] = 3;
-            problem.Structure[5, 0] = 4;
-            problem.Structure[5, 5] = 5;
-            problem.Structure[5, 6] = 9;
-            problem.Structure[6, 1] = 7;
-            problem.Structure[6, 3] = 3;
-            problem.Structure[7, 0] = 5;
-            problem.Structure[7, 8] = 8;
-            problem.Structure[8, 1] = 8;
-            problem.Structure[8, 4] = 2;
-            problem.Structure[8, 5] = 9;
-            problem.Structure[8, 7] = 6;
+            var problem = new Square(3)
+            {
+                Structure =
+                {
+                    [1, 3] = 7,
+                    [1, 5] = 1,
+                    [1, 8] = 2,
+                    [2, 2] = 9,
+                    [2, 7] = 4,
+                    [3, 0] = 8,
+                    [3, 1] = 1,
+                    [3, 5] = 7,
+                    [3, 8] = 4,
+                    [4, 3] = 2,
+                    [4, 4] = 4,
+                    [4, 6] = 3,
+                    [5, 0] = 4,
+                    [5, 5] = 5,
+                    [5, 6] = 9,
+                    [6, 1] = 7,
+                    [6, 3] = 3,
+                    [7, 0] = 5,
+                    [7, 8] = 8,
+                    [8, 1] = 8,
+                    [8, 4] = 2,
+                    [8, 5] = 9,
+                    [8, 7] = 6
+                }
+            };
 
             var result = solver.Solve(problem);
 
-            if (result.Solved)
-                Console.WriteLine(result.Square.ToString());
-            else
-            {
-                Console.WriteLine("Not solved");
-            }
+            Console.WriteLine(result.Solved ? result.Square.ToString() : "Not solved");
         }
     }
 }
